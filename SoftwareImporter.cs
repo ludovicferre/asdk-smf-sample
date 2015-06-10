8da04619102714410c78787aba9923447e5951b0
ludovicferre <ludovic.ferre@gmail.com>
2015-06-10 12:57:25 +0200

    Generic work ongoing
    That will require tidying up at some stage - once all we do in there is
    clear!

--- SoftwareImporter.cs
+++ SoftwareImporter.cs
@@ -1,104 +1,200 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
 using Altiris.ASDK.SMF;
+using Altiris.Common;
+using Altiris.Database;
+using Altiris.NS.ContextManagement;
 
 namespace Symantec.CWoC {
 	class SoftwareImporter {
+		public static readonly string ratguid_prodtocomp = "9D67B0C6-BEFF-4FCD-86C1-4A40028FE483";
+		public static readonly string prodtocomp = "Software Product Contains Software Component";
+
 		public static int Main() {
-		
+			string company_name = "Symantec CWoC";
+			string product_name = "Software Importer";
+			string product_description = "A software management program to create products from file";
+
+			string software_component_guid = "";
+
+			// Create the Software Product first
+			SoftwareProductManagementLib managementLib = new SoftwareProductManagementLib();
+			SoftwareProductDetails productDetails = managementLib.CreateSoftwareProduct(product_name, product_description, company_name);
+
+			Console.WriteLine("Company guid = {0}.", productDetails.CompanyGuid.ToString());
+			Console.WriteLine("Product name = {0}", productDetails.Name);
+			Console.WriteLine("Product guid = {0}", productDetails.Guid.ToString());
+
+			// Mark as managed in all cases (existing entry or not)
+			string _sql_manage_product = @"
+if exists (select 1 from Inv_Software_Product_State where _ResourceGuid = '{0}')
+	update Inv_Software_Product_State set IsManaged = 1 where _resourceguid = '{0}'
+else
+	insert Inv_Software_Product_State (_ResourceGuid, IsManaged) values ('{0}', 1)
+";
+			string sql_manage_product = String.Format(_sql_manage_product, productDetails.Guid);
+			DatabaseAPI.ExecuteNonQuery(sql_manage_product);
+
+			// Associate Software Component if needed
+			string _sql_associate_component = @"
+			";
+			string sql_associate_component = String.Format(_sql_associate_component, productDetails.Guid,);
+
+			// Add the inventory dataclass entries
+			string SoftwareNameQueryString = "";
+			string CompanyQueryString = "";
+			string VersionQueryString = "";
+
+			string _sql_inventory_query = @"
+SELECT	   component.Guid
+		  ,component.Name
+		  ,component.ResourceTypeGuid
+		  ,scVersion.Version
+		  ,company.Name As CompanyName 
+		  ,company.Guid AS CompanyGuid
+		  ,inst.Installs
+		  ,swState.IsManaged
+  FROM vRM_Software_Component_Item component
+  JOIN (
+		Select COUNT(DISTINCT _ResourceGuid) as Installs, _SoftwareComponentGuid
+		  FROM Inv_InstalledSoftware
+		 Where InstallFlag = 1
+		 Group By _SoftwareComponentGuid
+		) as inst 
+    ON inst._SoftwareComponentGuid = component.Guid
+  JOIN Inv_Software_Component_State swState
+    ON swState._ResourceGuid = component.Guid
+  LEFT JOIN (
+		SELECT vci.Guid, vci.Name, ra.ParentResourceGuid AS ComponentGuid
+		  FROM vRM_Company_Item vci
+		  JOIN ResourceAssociation ra
+		    ON ra.ChildResourceGuid = vci.Guid 
+		   AND ra.ResourceAssociationTypeGuid = '292dbd81-1526-423a-ae6d-f44eb46c5b16'
+		) company
+    ON company.ComponentGuid = component.Guid
+  LEFT JOIN Inv_Software_Component scVersion
+    ON scVersion._ResourceGuid = component.Guid
+ Where 1 = 1
+   AND Lower(component.Name) Like Lower('{0}')
+   AND Lower(ISNULL(company.Name, '')) Like Lower('{1}') 
+   AND Lower(ISNULL(scVersion.Version, '')) Like Lower('{2}') 
+ Order By component.Name Asc
+ ";
+
+			string sql_inventory_query = String.Format(_sql_inventory_query, SoftwareNameQueryString, CompanyQueryString, VersionQueryString);
+
+			// Create the data row for table Inv_SoftwareProductFilter
+
+			string _sql_software_product_filter = @"
+if exists (select 1 from Inv_SoftwareProductFilter where _ResourceGuid = '{0}')
+	update Inv_SoftwareProductFilter set NameFilter = '{1}', CompanyFilter = '{2}', VersionFilter = '{3}', SQLQueryFilter= '{4}' where _ResourceGuid = '{0}'
+else
+	insert Inv_SoftwareProductFilter (_ResourceGuid, NameFilter, CompanyFilter, VersionFilter, SQLQueryFilter)	values ('{0}', '{1}', '{2}', '{3}', '{4}')
+";
+			// Ensure we have a clear entry in the ResourceUpdateSummary table too
+/* 
 			// Software Release details
-			string SRName = "Orca 3.0b";
-			string SRDescription = "Software relesae resource for Orca 3.0";
+			SoftwareRelease sr = new SoftwareRelease();
+			sr.name = "Orca 3.0d";
+			sr.description = "Software release resource for Orca 3.0";
 			
 			// Software Package details
-			int SourceType = 2;
-			string PackageLocation = @"c:\packages\orca";
-			string PackageFolder = null;		// For url packages only
-			string InstallationFile = "Orca30.msi";
-			string Version = "3.0.1234";
+			SoftwarePackage pkg = new SoftwarePackage();
+			
+			pkg.sourcetype = (int) SoftwarePackage.SourceType.local;
+			pkg.location = @"c:\packages\orca";
+			pkg.installfile = "Orca30.msi";
+			pkg.version = "3.0.1234";
 			
 			// Generic data
-			string Company = "Microsoft";
-			string Product = "Orca (Product)";
+			SoftwareProduct prod = new SoftwareProduct();
+			prod.company = "Microsoft";
+			prod.product = "Orca (Product d)";
 			
-			// Import flags
+			ImportSoftware(sr, pkg, prod);
+*/
+			return 0;
+		}
+
+		public static void ImportSoftware (SoftwareRelease rel, SoftwarePackage pak, SoftwareProduct prod) {
 			int MergeAction = 0; // Create = 0; Merge = 1
-			
 			Guid ReleaseMergeTo = Guid.Empty; // Guid of release to merge to
-			
+
 			try {
 				SoftwareComponentManagementLib compLib = new SoftwareComponentManagementLib();
 				SoftwareComponentDetails details = compLib.ImportSoftwareRelease(
-					SRName,
-					SRDescription,
-					SourceType,
-					PackageLocation,
-					PackageFolder,
-					InstallationFile,
-					Version,
-					Company,
-					Product, 
+					rel.name,
+					rel.description,
+					pak.sourcetype,
+					pak.location,
+					pak.folder,
+					pak.installfile,
+					pak.version,
+					prod.company,
+					prod.product,
 					MergeAction,
 					ReleaseMergeTo);
-				
+
 				Console.WriteLine("Created software release with guid {0}", details.Guid.ToString());
-				
-			} catch (Exception e) {
+			}
+			catch (Exception e) {
 				Console.WriteLine(e.Message);
 				Console.WriteLine(e.StackTrace);
 			}
-
-			return 0;
 		}
 	}
-	
-	class SoftwareRelease {	
-	
+
+	class SoftwareRelease {
 		public SoftwareRelease () {
 			_name = "";
 			_description = "";
 		}
-		
+
 		public SoftwareRelease(string name, string description) {
 			this.name = name;
 			this.description = description;
 		}
-		
+
 		private string _name;
 		public string name {
 			get {
 				return _name;
 			}
+
 			set {
 				if (value.Length > 255)
 					throw new System.ArgumentException("Software Release name cannot be more than 255 character long.", "SoftwareRelease");
 				_name = value;
 			}
 		}
-		
+
 		private string _description;
 		public string description {
 			get {
 				return _description;
 			}
+
 			set {
 				if (value.Length > 255)
 					throw new System.ArgumentException("Software Release name cannot be more than 255 character long.", "SoftwareRelease");
 				_description = value;
-			
 			}
 		}
-		
 	}
-	
+
 	class SoftwarePackage {
 		public enum SourceType {
 			unc = 1,
 			local = 2,
 			url = 4,
-			library = 5			
+			library = 5
 		}
-		
+
 		public SoftwarePackage() {
 			_sourcetype = (int) SourceType.local;
 			_location = "";
@@ -113,16 +209,17 @@ namespace Symantec.CWoC {
 			if (folder == "" || folder == String.Empty || folder == null)
 				this.folder = null;
 			else
-				this.folder = folder;		
+				this.folder = folder;
 			this.installfile = installfile;
 			this.version = version;
 		}
-		
+
 		private int _sourcetype;
 		public int sourcetype {
 			get {
 				return _sourcetype;
 			}
+
 			set {
 				_sourcetype = value;
 			}
@@ -133,8 +230,9 @@ namespace Symantec.CWoC {
 			get {
 				return _location;
 			}
+
 			set {
-				if (_sourcetype == (int) SourceType.local &&  !Directory.Exists(value))
+				if (_sourcetype == (int) SourceType.local && !Directory.Exists(value))
 					throw new System.ArgumentException("Package location must be a valid folder on the server.", "SoftwarePackage");
 				_location = value;
 			}
@@ -145,6 +243,7 @@ namespace Symantec.CWoC {
 			set {
 				_folder = value;
 			}
+
 			get {
 				return _folder;
 			}
@@ -153,10 +252,11 @@ namespace Symantec.CWoC {
 		private string _installfile;
 		public string installfile {
 			set {
-				if (_sourcetype == (int) SourceType.local &&  !File.Exists(this.location + "\\" + value))
+				if (_sourcetype == (int) SourceType.local && !File.Exists(this.location + "\\" + value))
 					throw new System.ArgumentException("Install file must exists on the package folder on the server.", "SoftwarePackage");
 				_installfile = value;
 			}
+
 			get {
 				return _installfile;
 			}
@@ -167,40 +267,114 @@ namespace Symantec.CWoC {
 			set {
 				_version = value;
 			}
+
 			get {
 				return _version;
 			}
 		}
-		
 	}
 
 	class SoftwareProduct {
-	
 		public SoftwareProduct () {
 			_company = "";
 			_product = "";
+			_description = "";
 		}
-		public SoftwareProduct (string company, string product) {
+
+		public SoftwareProduct (string product, string company) {
 			this.company = company;
 			this.product = product;
+			this.description = "";
 		}
+
+		public SoftwareProduct (string product, string description, string company) {
+			this.company = company;
+			this.product = product;
+			this.description = description;
+		}
+
 		private string _company;
 		public string company {
 			get {
 				return _company;
 			}
+
 			set {
 				_company = value;
 			}
 		}
+
 		private string _product;
 		public string product {
 			get {
 				return _product;
 			}
+
 			set {
 				_product = product;
 			}
 		}
+
+		private string _description;
+		public string description {
+			get {
+				return _description;
+			}
+
+			set {
+				_description = value;
+			}
+		}
 	}
+
+	class DatabaseAPI {
+        public static DataTable GetTable(string sqlStatement) {
+            DataTable t = new DataTable();
+            try {
+                using (DatabaseContext context = DatabaseContext.GetContext()) {
+                    SqlCommand cmdAllResources = context.CreateCommand() as SqlCommand;
+                    cmdAllResources.CommandText = sqlStatement;
+
+                    using (SqlDataReader r = cmdAllResources.ExecuteReader()) {
+                        t.Load(r);
+                    }
+                }
+                return t;
+            }
+            catch {
+                throw new Exception("Failed to execute SQL command...");
+            }
+        }
+
+        public static int ExecuteNonQuery(string sqlStatement) {
+            try {
+                using (DatabaseContext context = DatabaseContext.GetContext()) {
+                    SqlCommand sql_cmd = context.CreateCommand() as SqlCommand;
+                    sql_cmd.CommandText = sqlStatement;
+
+                    return sql_cmd.ExecuteNonQuery();
+                }
+            } catch {
+                throw new Exception("Failed to execute non query SQL command...");
+            }
+
+        }
+
+        public static int ExecuteScalar(string sqlStatement) {
+            try {
+                using (DatabaseContext context = DatabaseContext.GetContext()) {
+                    SqlCommand cmd = context.CreateCommand() as SqlCommand;
+
+                    cmd.CommandText = sqlStatement;
+                    Object result = cmd.ExecuteScalar();
+
+                    return Convert.ToInt32(result);
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Error: {0}\nException message = {1}\nStack trace = {2}.", e.Message, e.InnerException, e.StackTrace);
+                throw new Exception("Failed to execute scalar SQL command...");
+            }
+        }
+    }
+
 }
--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 
 set smf=Altiris.ASDK.SMF
+set ans=Altiris.NS
+set adb=Altiris.Database
+set acm=Altiris.Common
 
 
 if "%1"=="7.6" goto build-7.6
@@ -11,7 +14,11 @@ if "%1"=="7.1" goto build-7.1
 set build=7.5
 set gac=C:\Windows\Assembly\GAC_MSIL
 set csc=@c:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe
-set ver=7.5.3083.0__d516cb311cfb6e4f
+
+set ver1=7.5.3153.0__d516cb311cfb6e4f
+set ver2=7.5.3219.0__d516cb311cfb6e4f
+set ver3=7.5.3083.0__d516cb311cfb6e4f
+set ver4=7.5.3153.0__99b1e4cc0d03f223
 
 
 goto build
@@ -22,7 +29,10 @@ set build=7.1
 set gac=C:\Windows\Assembly\GAC_MSIL
 set csc=@c:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe
 
-set ver=7.1.8400.0__d516cb311cfb6e4f
+set ver1=7.1.8400.0__d516cb311cfb6e4f
+set ver2=7.1.7858.0__d516cb311cfb6e4f
+set ver3=7.1.8400.0__99b1e4cc0d03f223
+set ver4=7.1.8400.0__d516cb311cfb6e4f
 
 
 goto build
@@ -34,11 +44,13 @@ set build=7.6
 set gac=C:\Windows\Microsoft.NET\assembly\GAC_MSIL
 set csc=@c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 
-set ver=v4.0_7.6.1383.0__d516cb311cfb6e4f
+set ver1=v4.0_7.6.1383.0__d516cb311cfb6e4f
+set ver2=v4.0_7.6.1395.0__d516cb311cfb6e4f
+set ver3=v4.0_7.6.1383.0__99b1e4cc0d03f223
 
 
 
 :build
-cmd /c %csc% /reference:%gac%\%smf%\%ver%\%smf%.dll /out:SoftwareImporter-%build%.exe *.cs
+cmd /c %csc% /reference:%gac%\%smf%\%ver3%\%smf%.dll /reference:%gac%\%ans%\%ver1%\%ans%.dll /reference:%gac%\%acm%\%ver1%\%acm%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /out:SoftwareImporter-%build%.exe *.cs
 
 :end
