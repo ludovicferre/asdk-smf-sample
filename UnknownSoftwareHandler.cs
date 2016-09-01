4cce96f600c5022c59e7ca516c262f057ba80ae4
ludovicferre <ludovic.ferre@gmail.com>
2016-09-01 16:47:05 +0200

    Various updates regarding /export and general handling.

--- UnknownSoftwareHandler.cs
+++ UnknownSoftwareHandler.cs
@@ -46,11 +46,9 @@ IMPORT and CREATION:
 		
 		public static int Main(string [] Args) {
 
-			CLIConfig conf;
+			CLIConfig conf = GetCLIConfig(Args);
 			
-			if (Args.Length > 0) {
-				conf = GetCLIConfig(Args);
-			} else {
+			if (conf.display_help) {
 				Console.WriteLine(HELP_MSG);
 				return 0;
 			}
@@ -58,6 +56,8 @@ IMPORT and CREATION:
 			if (conf.export_mode == true) {
 				// Craft the export SQL based on the CLI configuration
 				string unk_sql = String.Format(sql.undefined_software_query_base, conf.company_filter, conf.company_name);
+				
+				// Console.WriteLine(unk_sql);
 				DataTable unknown_components = DatabaseAPI.GetTable(unk_sql);
 
 				// Open the output file for writing as UTF-8
@@ -85,11 +85,16 @@ IMPORT and CREATION:
 				}
 			
 				writer.Close();
-			return 0;
+				return 0;
 			} else {
 				// Import data from CSV and run the product creation based on that.
 				string path = "input.csv";
 				string [] in_data;
+				
+				int i = 0;
+				DateTime t = DateTime.Now;
+				string description = "";
+				
 				if (File.Exists(path)) {
 					using (StreamReader sr = new StreamReader(path)) {
 						while (sr.Peek() >= 0) {
@@ -97,8 +102,30 @@ IMPORT and CREATION:
 							if (in_data[0] == "\"component_name\"") {
 								continue;
 							}
-							create_software_product(in_data[1].Replace("\"", ""), "CWoC created product", in_data[2].Replace("\"", ""), in_data[3].Replace("\"", ""), in_data[8].Replace("\"", ""), in_data[4].Replace("\"", ""));
-							// Console.WriteLine("{0} {1} {2} {3} {4} {5}", in_data[1].Replace("\"", ""), "CWoC created product", in_data[2], in_data[3], in_data[8], in_data[4]);
+							i++;
+							description = String.Format("Created by UnknownSoftwareHandler.exe on {0}, entry id in batch={1}", t.ToString(), i.ToString());
+							if (conf.dryrun) {
+								Console.WriteLine("\n\tProduct\t{0}\n\tDescr.\t{1}\n\tFilter\t{2}\n\tVersion\t{3}\n\tCompany\t{5}\n\tGuid\t{4}\n"
+									, in_data[1].Replace("\"", "")	// Product
+									, description					// Description
+									, in_data[2].Replace("\"", "")	// Filter
+									, in_data[3].Replace("\"", "")  // Version
+									, in_data[8].Replace("\"", "")	// Guid
+									, in_data[4].Replace("\"", "")	// Company
+								);
+								continue;
+							}
+							if (conf.testrun && i > 10) {
+								break;
+							}
+							create_software_product(
+								  in_data[1].Replace("\"", "")	// Product
+								, description					// Description
+								, in_data[2].Replace("\"", "")	// Filter
+								, in_data[3].Replace("\"", "")  // Version
+								, in_data[8].Replace("\"", "")	// Guid
+								, in_data[4].Replace("\"", "")	// Company
+							);
 						}
 					}
 				return 0;
@@ -107,50 +134,6 @@ IMPORT and CREATION:
 					return - 1;
 				}
 			}
-/*	SKIP ALL PRODUCT GENERATION FOR TESTING PURPOSES
-
-			#region Main work take place here #TODO: Export this to a function?
-			// Create the Software Product first and mark as managed
-			SoftwareProductManagementLib managementLib = new SoftwareProductManagementLib();
-			SoftwareProductDetails productDetails = managementLib.CreateSoftwareProduct(conf.product_name, conf.product_description, conf.company_name);
-
-			Console.WriteLine("Company guid = {0}.", productDetails.CompanyGuid.ToString());
-			Console.WriteLine("Product name = {0}", productDetails.Name);
-			Console.WriteLine("Product guid = {0}", productDetails.Guid.ToString());
-
-			string sql_manage_product = String.Format(sql.set_product_managed, productDetails.Guid);
-			DatabaseAPI.ExecuteNonQuery(sql_manage_product);
-
-            string sql_mark_component_managed = String.Format("update Inv_Software_Component_State set IsManaged = 1 where _ResourceGuid = '{0}'", conf.software_component_guid);
-            DatabaseAPI.ExecuteNonQuery(sql_mark_component_managed);
-
-
-			// Associate Software Component if needed
-			if (conf.software_component_guid != "") {
-				string sql_associate_component = String.Format(sql.associate_component, conf.software_component_guid, productDetails.Guid.ToString());
-				// Console.WriteLine("\n" + sql_associate_component + "\n");
-				DatabaseAPI.ExecuteNonQuery(sql_associate_component);
-			} else {
-				// Console.WriteLine("SOWFTWARE COMPONENT GUID IS MISSING");
-			}
-			
-			if (conf.product_version != "") {
-				string sql_insert_version = String.Format(sql.ins_product_version, productDetails.Guid, conf.product_version);
-				DatabaseAPI.ExecuteNonQuery(sql_insert_version);
-			}
-			
-			// We only add filters if _both_ the company and product are present in the config data
-			if (conf.filter_product != "" && conf.filter_company != "") {
-				
-				string prod_filter = GetProductNameFilter(conf.filter_product);
-				string sql_filter = String.Format(sql.product_filter_query, conf.filter_product, conf.filter_company, conf.filter_version);
-				
-				string sql_insert_filter = String.Format(sql.ins_product_filter, productDetails.Guid, conf.filter_product, conf.filter_company, conf.filter_version, sql_filter);
-				
-				DatabaseAPI.ExecuteNonQuery(sql_insert_filter);
-			}
-			#endregion
-*/			
 		}
 
 		public static void create_software_product(string product, string description, string filter, string version, string guid, string company) {
@@ -187,6 +170,7 @@ IMPORT and CREATION:
 				DatabaseAPI.ExecuteNonQuery(sql_insert_filter);
 			}
 		}
+
 		public class compute_product_data {
 			public string product_name;
 			public string product_description;
@@ -332,6 +316,10 @@ IMPORT and CREATION:
 					conf.export_mode = false;
 				} else if (arg =="/import") {
 					conf.export_mode = false;
+					conf.display_help = false;
+				} else if (arg =="/export") {
+					conf.export_mode = true;
+					conf.display_help = false;
 				} else if (arg =="/version_exact") {
 					conf.versioning_mode = Versioning.Exact;
 				} else if (arg == "/version_major") {
@@ -344,8 +332,14 @@ IMPORT and CREATION:
 					conf.company_name = arg.Substring("/corpname=".Length);
 				} else if (arg.StartsWith("/corpfilter=")) {
 					conf.company_filter = arg.Substring("/corpfilter=".Length);
+				} else if (arg == "/?" || arg == "/help") {
+					conf.display_help = true;
 				}
 			}
+			if (conf.company_filter == "" && conf.company_name == "") {
+				conf.company_filter="%";
+			}
+			
 			return conf;
 		}
 	}
@@ -366,11 +360,11 @@ IMPORT and CREATION:
 		
 		public CLIConfig() {
 			// Initialise the config properties
-			display_help = false;
+			display_help = true;
 			export_mode = true;
 
 			company_name = "";
-			company_filter = "%";
+			company_filter = "";
 			
 			dryrun= false;
 			testrun = false;
